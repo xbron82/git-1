f270673f1a3c09a925d6dd769d7687e164cfce78
unknown <xbron82@gmail.com>
2026-01-11 11:11:21 +0000

    save-1

---
+++ program.py
@@ -0,0 +1,8 @@
+import random as r 
+
+r_number = r.randint(1, 100)
+u_number= int(input("enter a number"))
+
+new_num=r_number+u_number
+print("The random number is:", r_number)
+print("The sum of the random number and your number is:", new_num)
